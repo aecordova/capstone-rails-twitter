c84283fd4ae1a686f826188ad12ad74dd5199b37
acordova <ae_cordova@outlook.com>
2020-04-08 21:35:58 -0500

    added index method to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,27 @@
 class PostsController < ApplicationController
+  def index
+    timeline_posts  
+  end
+
   def create
     @post = current_user.posts.build(post_params)
+    puts "Valid post?" + @post.valid?.to_s
+    if @post.save
+      puts "Post saved:" + @post.content.to_s
+      redirect_to home_path
+    else
+      render html:"Post not created"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def timeline_posts
+    
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :posts
   has_secure_password
+
+  def timeline_posts
+     
+  end
 end
